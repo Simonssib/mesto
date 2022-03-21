996fcbe199a52372add0a9d4cac1fcce905f7962
Simon Alexeev <alekseev-simon@mail.ru>
2022-03-22 00:52:28 +0300

    refactor: correction main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,22 +6,22 @@ const postPopup = document.querySelector('.popup-post');
 const postPopupOpen = document.querySelector('.profile__button-add');
 const postPopupClose = postPopup.querySelector('.popup__close');
 
-let profileName = document.querySelector('.profile__name');
-let profileProfession = document.querySelector('.profile__profession');
-let popupName = document.querySelector('.popup__field_text_name');
-let popupProfession = document.querySelector('.popup__field_text_profession');
-const saveButton = document.querySelector('.popup__save');
+const profileName = document.querySelector('.profile__name');
+const profileProfession = document.querySelector('.profile__profession');
+const popupName = document.querySelector('.popup__field_text_name');
+const popupProfession = document.querySelector('.popup__field_text_profession');
+const buttonSave = document.querySelector('.popup__save');
 const likeButtonList = document.querySelectorAll('.elements__button-like');
-let form = document.querySelector('.popup__input');
+const formProfile = profilePopup.querySelector('.popup__input');
 
 const cardTemplate = document.querySelector('.elements-template');
-const addButton = postPopup.querySelector('.popup__save');
+const buttonAdd = postPopup.querySelector('.popup__save');
 const cardContainer = document.querySelector('.elements');
 const inputCardTitle = postPopup.querySelector('.popup__field_text_title');
 const inputCardLink = postPopup.querySelector('.popup__field_text_link');
 const formCard = postPopup.querySelector('.popup__input');
 const card = postPopup.querySelector('.elements__item');
-const closeButtonImage = cardTemplate.content.querySelector('.popup__close');
+const buttonCloseImage = cardTemplate.content.querySelector('.popup__close');
 
 const initialCards = [{
         name: 'Архыз',
@@ -128,5 +128,5 @@ postPopupClose.addEventListener('click', () => closePopup(postPopup));
 popupZoomClose.addEventListener('click', () => closePopup(popupZoom));
 
 
-form.addEventListener('submit', formSubmitHandler);
+formProfile.addEventListener('submit', formSubmitHandler);
 formCard.addEventListener('submit', addCard);
