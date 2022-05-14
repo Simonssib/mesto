7ba44ba7cffc758e75ccb7dffeca8b75732524ee
Simon Alexeev <alekseev-simon@mail.ru>
2022-05-14 17:19:58 +0300

    fix: change PopupWithImages

--- src/components/PopupWithImages.js
+++ src/components/PopupWithImages.js
@@ -3,8 +3,8 @@ import Popup from "../components/Popup.js";
 export default class PopupWithImage extends Popup {
     constructor({ popupSelector, imageSelector, titleSelector }) {
         super(popupSelector);
-        this._image = document.querySelector(imageSelector);
-        this._title = document.querySelector(titleSelector);
+        this._image = this._popup.querySelector(imageSelector);
+        this._title = this._popup.querySelector(titleSelector);
     }
     open(name, link) {
         this._image.src = link;
